b8e3ee42d917fbdf59bdf416d48011a03e8d8046
shota.ushiro <deeper0120@icloud.com>
2020-06-01 12:40:01 +0000

    add colum

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+    
+    validates :content, presence: true, length: { maximum: 255 }
+    
+    validates :status, presence: true, length: { maximum: 10 }
+    
 end
