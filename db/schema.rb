58bf28abaaeb05560ff25f6bf622e48f8c33bbff
shota.ushiro <deeper0120@icloud.com>
2020-06-17 07:09:13 +0000

    kakunin

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 2020_06_12_151226) do
 
   create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
