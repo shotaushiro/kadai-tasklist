1ad46ae00cb8d060f340a55c35c8fe22400ac4f6
shota.ushiro <deeper0120@icloud.com>
2020-06-18 04:00:34 +0000

    migrate remake

--- db/migrate/20200618034935_add_user_to_tasks.rb
+++ db/migrate/20200618034935_add_user_to_tasks.rb
@@ -1,11 +1,9 @@
 class AddUserToTasks < ActiveRecord::Migration[5.2]
   def change
-    create_table :tasks do |t|
+    create_table :microposts do |t|
       t.string :content
-      
-      add_reference :user, foreign_key: true
-    
+    add_reference :tasks, :user, foreign_key: true
     t.timestamps
-    end
+  end
   end
 end
