1510823cd1707fad51ce53e8cbd7a35ca0236380
shota.ushiro <deeper0120@icloud.com>
2020-06-20 11:53:35 +0000

    unnecessary delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,11 +3,11 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy, :show, :edit, :update]
   def index
-    if logged_in?
+    #if logged_in?
       #@task = current_user.tasks.build
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       
-    end
+    #end
   end
 
   def show
@@ -18,7 +18,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    #@task = Task.new(task_params)
     @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task を投稿しました。'
